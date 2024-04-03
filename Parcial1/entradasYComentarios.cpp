3cc8abf80582e59d158568cf69a229d03546eecf
FelipeDosantosG <164356071+FelipeDosantosG@users.noreply.github.com>
2024-04-03 09:01:26 -0500

    Sexto commit
    Se terminaron de estructurar las funciones encargadas de crear rotar matrices, también se limpió el código y se creó un archivo nuevo en el cuál se guardan comentarios de fragmentos de código que pueden ser utilizados en la rama principal, como validaciones imputs, etc.

---
+++ Parcial1/entradasYComentarios.cpp
@@ -0,0 +1,29 @@
+    /*cout << "Ingrese el valor de la llave k";
+    cout << "Ingrese un número impar para el tamaño de la matriz 1: ";
+    cin >> n1;
+    cout << "Ingrese un número impar para el tamaño de la matriz 2: ";
+    cin >> n2;
+    cout << "Ingrese un número impar para el tamaño de la matriz 3: ";
+    cin >> n3;
+    cout << "Ingrese un número impar para el tamaño de la matriz 4: ";
+    cin >> n4;
+
+    cout << "Ingrese el valor de la fila: ";
+    cin >> fil;
+    cout << "Ingrese el valor de la columna: ";
+    cin >> col;*/
+   
+    /*if ((n1 % 2 == 0) or (n2 % 2 == 0) or (n3 % 2 == 0) or (n4 % 2 == 0)) {
+    cout << "El número ingresado no es impar. Por avor, intente de nuevo con un número impar." << endl;
+    return 1;
+    } Esta es una validación que se asegura que los datos sean impares.*/
+   
+    //hacerMatriz(matriz, n2, &matrix, fil, col);
+    //cout << "El valor del return: " << matrix << endl;
+   
+    /*hacerMatriz(matriz, n3, &matrix, fil, col);
+    cout << "El valor del return: " << matrix << endl << endl;
+    hacerMatriz(matriz, n4, &matrix, fil, col);
+    cout << "El valor del return: " << matrix << endl << endl;
+    */
+    //cout << "El primer dato al que se refiere la llave es: " << keyV << endl;
--- Parcial1/fMatrices.cpp
+++ Parcial1/fMatrices.cpp
@@ -2,9 +2,9 @@
 
 using namespace std;
 
-const int MAX = 10;
+const int MAX = 15;
 
-void rotarMatriz(int matriz[MAX][MAX], int n) {
+void rotarMatriz(int matriz[MAX][MAX], int n, int* matrixR, int fil, int col) {
     // Crear una matriz auxiliar para almacenar la matriz rotada
     int matriz_rotada[MAX][MAX];
 
@@ -14,6 +14,8 @@ void rotarMatriz(int matriz[MAX][MAX], int n) {
             matriz_rotada[n - 1 - j][i] = matriz[i][j];
         }
     }
+
+    *matrixR = matriz_rotada[fil][col];
 
     // Copiar la matriz rotada de vuelta a la matriz original
     for (int i = 0; i < n; ++i) {
@@ -37,9 +39,7 @@ void rotarMatriz(int matriz[MAX][MAX], int n) {
 }
 
 void hacerMatriz(int matriz[MAX][MAX], int n, int* matrix, int fil, int col){
-
     int valor = 1;
-    //int matrix; 
 
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
@@ -64,71 +64,24 @@ void hacerMatriz(int matriz[MAX][MAX], int n, int* matrix, int fil, int col){
             }
         }
         cout << endl;
-    }
-    
+    } 
 }
 
 int main() {
-    int n1, n2, n3, n4, keyV, fil, col; //n1 representa el tamaño de la matriz 1
-
-    cout << "Ingrese el valor de la llave k";
-    cout << "Ingrese un número impar para el tamaño de la matriz 1: ";
-    cin >> n1;
-    cout << "Ingrese un número impar para el tamaño de la matriz 2: ";
-    cin >> n2;
-    cout << "Ingrese un número impar para el tamaño de la matriz 3: ";
-    cin >> n3;
-    cout << "Ingrese un número impar para el tamaño de la matriz 4: ";
-    cin >> n4;
-
-    cout << "Ingrese el valor de la fila: ";
-    cin >> fil;
-    cout << "Ingrese el valor de la columna: ";
-    cin >> col;
-
-    if ((n1 % 2 == 0) or (n2 % 2 == 0) or (n3 % 2 == 0) or (n4 % 2 == 0)) {
-        cout << "El número ingresado no es impar. Por favor, intente de nuevo con un número impar." << endl;
-        return 1;
-    }
-
-
-
-    int matriz[MAX][MAX], matrix;
-
     
-
+    int n1 = 3, n2 = 5, fil = 2, col = 2, matriz[MAX][MAX], matrix, matrixR; //n1 representa el tamaño de la matriz 1
 
     hacerMatriz(matriz, n1, &matrix, fil, col);
-    keyV = matrix;
+
     cout << "El valor del return: " << matrix << endl << endl;
 
-    hacerMatriz(matriz, n2, &matrix, fil, col);
-    cout << "El valor del return: " << matrix << endl;
-
     if (matrix == 0){
-        cout << "El tamaño de la matriz debería ser diferente.  \n \n";
+        cout << "El tamaño rotarMatriz(matriz, n1) debería ser diferente.  \n \n";
     } else {
         cout << "El tamaño de la matriz está bién. \n \n";
     }
-    
-    hacerMatriz(matriz, n3, &matrix, fil, col);
-    cout << "El valor del return: " << matrix << endl << endl;
-    hacerMatriz(matriz, n4, &matrix, fil, col);
-    cout << "El valor del return: " << matrix << endl << endl;
 
-    cout << "El primer dato al que se refiere la llave es: " << keyV << endl;
-
-
-    /*rotarMatriz(matriz, n1);
-
-    hacerMatriz(matriz, n2);
-    rotarMatriz(matriz, n2);
-
-    hacerMatriz(matriz, n3);
-    rotarMatriz(matriz, n2);
-
-    hacerMatriz(matriz, n4);
-    rotarMatriz(matriz, n4);*/
+    rotarMatriz(matriz, n1, &matrixR, fil, col);
    
     return 0;
 }
